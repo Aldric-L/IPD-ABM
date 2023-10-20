9e93533c39dab39c7f9d54b32aa9ec9a8f738f63
Aldric-L <al9.labarthe@free.fr>
2023-10-20 10:15:24 +0200

    Initial Commit

---
+++ IPD-ABM/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  IPD-ABM
+//
+//  Created by Aldric Labarthe on 20/10/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
